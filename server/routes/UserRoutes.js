326a83d5853c2f96f1be7ade7006eff983fa0c60
Zakaria <zaktakk96@gmail.com>
2022-07-06 22:29:08 +0300

    Backend: Implemented User routes

---
+++ server/routes/UserRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { register, login } = require("../controller/UserController");
+
+// Register Route
+
+router.post("/register", register);
+
+// Login Route
+
+router.post("/login", login);
+
+module.exports = router;
