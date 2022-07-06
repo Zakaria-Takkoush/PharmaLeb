7fd89d9004366b084bd3d66ce5fe849c5f604e54
Zakaria <zaktakk96@gmail.com>
2022-07-06 22:05:13 +0300

    Backend: Created Pharmacy model schema

---
+++ server/model/PharmacyModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const pharmacySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    address: { type: String, required: true },
+    owner: { type: mongoose.Types.ObjectId, ref: "User" },
+    location: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+    },
+    items: [
+        {
+            item: {
+                type: mongoose.Types.ObjectId,
+                ref: "Medicine",
+            },
+            stock: { type: Number },
+        },
+    ],
+});
+
+module.exports = mongoose.model("Pharmacy", pharmacySchema);
