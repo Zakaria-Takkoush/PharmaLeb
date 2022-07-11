c5d765ca3df2c278b599f8d6a727af753701c952
Zakaria <zaktakk96@gmail.com>
2022-07-11 17:47:02 +0300

    Backend: Removed Meaages model, to be implemented through firebase

--- server/model/MessageModel.js
+++ server/model/MessageModel.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-    // 1st party
-    user: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-    },
-    // 2nd party
-    pharmacy: {
-        type: mongoose.Types.ObjectId,
-        ref: "Pharmacy",
-    },
-    content: {
-        type: String,
-    },
-});
+// const messageSchema = new mongoose.Schema({
+//     // 1st party
+//     user: {
+//         type: mongoose.Types.ObjectId,
+//         ref: "User",
+//     },
+//     // 2nd party
+//     pharmacy: {
+//         type: mongoose.Types.ObjectId,
+//         ref: "Pharmacy",
+//     },
+//     content: {
+//         type: String,
+//     },
+// });
 
-module.exports = mongoose.model("Message", messageSchema);
+// module.exports = mongoose.model("Message", messageSchema);
