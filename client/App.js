95e043ab283e232a1fe41237c5a4db3021180923
Zakaria <zaktakk96@gmail.com>
2022-07-13 13:03:20 +0300

    Frontend: Polished login screen

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
     Image,
     StyleSheet,
@@ -6,32 +7,45 @@ import {
     View,
     TouchableOpacity,
     TextInput,
+    Button,
 } from "react-native";
 // logo
 import logo from "./assets/logo/Logo.jpg";
 
 export default function App() {
+    const [user, setUser] = useState({});
+
     return (
         <View style={styles.container}>
+            {/* logo */}
             <Image source={logo} style={styles.logo} />
+            {/* login form */}
             <View style={styles.form}>
                 <Text style={styles.label}>Email:</Text>
-                <View style={styles.input}>
-                    <TextInput></TextInput>
-                </View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your email..."
+                    onChangeText={(value) => {
+                        setUser({ ...user, email: value });
+                    }}
+                />
                 <Text style={styles.label}>Password:</Text>
-                <View style={styles.input}>
-                    <TextInput></TextInput>
-                </View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your password..."
+                    secureTextEntry={true} // password
+                    onChangeText={(value) => {
+                        setUser({ ...user, password: value });
+                    }}
+                />
                 <Text style={styles.forgot}>Forgot password?</Text>
             </View>
-            <TouchableOpacity onPress={() => alert("")} style={styles.button}>
+            {/* sign in button */}
+            <TouchableOpacity style={styles.button}>
                 <Text style={styles.button_text}>Sign in</Text>
             </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => alert("")}
-                style={styles.button_green}
-            >
+            {/* register button */}
+            <TouchableOpacity style={styles.button_green}>
                 <Text style={styles.button_text}>Sign Up!</Text>
             </TouchableOpacity>
         </View>
@@ -80,7 +94,7 @@ const styles = StyleSheet.create({
     form: {},
     input: {
         borderColor: "#959595",
-        borderWidth: 2,
+        borderWidth: 1,
         borderRadius: 10,
         width: 300,
         padding: 10,
