1d842b5837df08a773fa72b9f2646c2381b3a3c7
Zakaria <zaktakk96@gmail.com>
2022-07-14 00:02:09 +0300

    Frontend: Created screens folder and added SignUp screen

--- client/components/PharmacyCard.js
+++ client/components/PharmacyCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export const PharmacyCard = () => {
---
+++ client/screens/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TouchableWithoutFeedback, View } from "react-native";
+import { BlueButton } from "../components/BlueButton";
+
+export const SignUp = () => {
+    return (
+        <TouchableWithoutFeedback>
+            <View>SignUp</View>
+            <BlueButton />
+        </TouchableWithoutFeedback>
+    );
+};
+
+const styles = StyleSheet.create({});
