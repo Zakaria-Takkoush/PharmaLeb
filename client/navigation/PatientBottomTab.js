6ba202d5571cfde21825bf3820b9c50d4bee821c
Zakaria <zaktakk96@gmail.com>
2022-07-16 22:46:50 +0300

    Frontend: Added bottom tab navigation icons

--- client/navigation/PatientBottomTab.js
+++ client/navigation/PatientBottomTab.js
@@ -4,16 +4,59 @@ import { Favorites } from "../screens/Patient/Favorites";
 import { Home } from "../screens/Patient/Home";
 import { Profile } from "../screens/Patient/Profile";
 import { FontAwesome } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export const PatientBottomTab = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Favorites" component={Favorites} />
-            <Tab.Screen name="Chats" component={Chat} />
-            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Entypo name="home" size={35} color="#959595" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Favorites"
+                component={Favorites}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome name="star" size={35} color="#959595" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Chats"
+                component={Chat}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name="chatbubble-ellipses"
+                            size={35}
+                            color="#959595"
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <FontAwesome5
+                            name="user-alt"
+                            size={35}
+                            color="#959595"
+                        />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 };
