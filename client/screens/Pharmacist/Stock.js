ad4efbbd27c26f6efc1b716daf8922e2f7e78057
Zakaria <zaktakk96@gmail.com>
2022-07-17 22:24:57 +0300

    Frontend: Added content to stock management menu for pharmacist

--- client/screens/Pharmacist/Stock.js
+++ client/screens/Pharmacist/Stock.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, Text, ScrollView, View } from "react-native";
 import React from "react";
+
+// import medicine edit card
 import { MedicineCardStock } from "../../components/MedicineCardStock";
+
+// import global styles
+import globalStyles from "../../styles/GlobalStyles";
+
+// import blue button
+import { BlueButton } from "../../components/BlueButton";
 
 export const Stock = () => {
     return (
-        <View>
-            <MedicineCardStock />
-            <MedicineCardStock />
+        <View style={globalStyles.pageContainer}>
+            <View style={styles.search}>
+                <TextInput
+                    style={globalStyles.input}
+                    placeholder="Search items..."
+                />
+                <BlueButton text="Search" />
+            </View>
+            <ScrollView style={globalStyles.itemList}>
+                <MedicineCardStock />
+                <MedicineCardStock />
+                <MedicineCardStock />
+                <MedicineCardStock />
+            </ScrollView>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    search: {
+        paddingHorizontal: 30,
+        alignSelf: "stretch",
+    },
+});
