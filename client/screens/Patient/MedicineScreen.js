55b3d8e1cc5eb8fa211a7c6f0ad10167ed104522
Zakaria <zaktakk96@gmail.com>
2022-07-17 19:02:05 +0300

    Frontend: Added dummy content and styling to medicine screen

--- client/screens/Patient/MedicineScreen.js
+++ client/screens/Patient/MedicineScreen.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
+import { MedicineScreenDetails } from "../../components/MedicineScreenDetails";
+import { PharmacyCard } from "../../components/PharmacyCard";
+import globalStyles from "../../styles/GlobalStyles";
 
 const MedicineScreen = () => {
     return (
-        <View>
-            <Text>MedicineScreen</Text>
+        <View style={globalStyles.pageContainer}>
+            <MedicineScreenDetails />
+            <Text style={styles.text}>Available at:</Text>
+            <ScrollView style={globalStyles.itemList}>
+                <PharmacyCard />
+                <PharmacyCard />
+                <PharmacyCard />
+                <PharmacyCard />
+            </ScrollView>
         </View>
     );
 };
 
 export default MedicineScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        alignSelf: "flex-start",
+        marginLeft: 35,
+    },
+});
