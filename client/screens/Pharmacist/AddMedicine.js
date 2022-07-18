44d845ffa819641c1592b8e4e94a04abdd6eba91
Zakaria <zaktakk96@gmail.com>
2022-07-18 11:21:18 +0300

    Frontend: Created add item to stock screen

---
+++ client/screens/Pharmacist/AddMedicine.js
@@ -0,0 +1,39 @@
+import { StyleSheet, TextInput, Text, ScrollView, View } from "react-native";
+import React from "react";
+
+// import add item card
+import { AddItemCard } from "../../components/AddItemCard";
+
+// import global styles
+import globalStyles from "../../styles/GlobalStyles";
+
+// Import blue button
+import { BlueButton } from "../../components/BlueButton";
+
+export const Stock = () => {
+    return (
+        <View style={globalStyles.pageContainer}>
+            <View style={styles.search}>
+                <TextInput
+                    style={globalStyles.input}
+                    placeholder="Search items..."
+                />
+                <BlueButton text="Search" />
+            </View>
+            <ScrollView style={globalStyles.itemList}>
+                <AddItemCard />
+                <AddItemCard />
+                <AddItemCard />
+                <AddItemCard />
+                <AddItemCard />
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    search: {
+        paddingHorizontal: 30,
+        alignSelf: "stretch",
+    },
+});
