6562b576fe11a2fa36cb514667fcf950cc4c3fdb
Zakaria <zaktakk96@gmail.com>
2022-07-18 14:26:35 +0300

    Frontend: Added content and styled medicine stock screen

--- client/screens/Pharmacist/MedicineStock.js
+++ client/screens/Pharmacist/MedicineStock.js
@@ -1,12 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+
+// import item details component
+import { ItemScreenDetails } from "../../components/ItemScreenDetails";
+
+// import global styles
+import globalStyles from "../../styles/GlobalStyles";
+
+// import icons
+import { Ionicons } from "@expo/vector-icons";
 
 export const MedicineStock = () => {
+    const [stock, setStock] = useState(0);
+    const increment = () => {
+        setStock(stock + 1);
+    };
+    const decrement = () => {
+        setStock(stock - 1);
+    };
+
     return (
-        <View>
-            <Text>MedicineStock</Text>
+        <View style={globalStyles.pageContainer}>
+            <ItemScreenDetails />
+            <Text style={styles.text}>Stock: {stock}</Text>
+            <View style={styles.editStock}>
+                <Ionicons
+                    name="ios-remove-circle"
+                    size={55}
+                    color="tomato"
+                    onPress={decrement}
+                />
+                <TextInput
+                    style={[styles.inputStock, globalStyles.input]}
+                    onChangeText={(value) => setStock(parseInt(value))}
+                    vale={stock}
+                    keyboardType="numeric"
+                />
+                <Ionicons
+                    name="ios-add-circle"
+                    size={55}
+                    color="green"
+                    onPress={increment}
+                />
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        alignSelf: "flex-start",
+        marginLeft: 50,
+    },
+    editStock: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 50,
+        marginVertical: 15,
+    },
+    inputStock: {
+        fontSize: 20,
+        flex: 1,
+    },
+});
