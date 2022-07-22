8c03c8ed93752eca1edd913c820ba67249763e52
Zakaria <zaktakk96@gmail.com>
2022-07-22 11:24:49 +0300

    Linking: Linked medicine availability api

--- client/components/PharmacyCard.js
+++ client/components/PharmacyCard.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-export const PharmacyCard = () => {
+export const PharmacyCard = ({ details, medicine }) => {
+    // console.log(medicine);
+    // console.log(details);
+
+    // const [itemStock, setItemStock] = useState();
+
+    // Loop through items of the pharmacy and find the stock of this medicine
+    let stock = 0;
+    let items = details.items;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].item === medicine) {
+            stock = items[i].stock;
+        }
+    }
+    // const index = items.findIndex((item) => {
+    //     return item.item === medicine;
+    // });
+    // setItemStock(items[index].stock);
+
     return (
         <TouchableOpacity style={styles.card}>
-            <Text style={styles.name}>Pharmacy Name</Text>
+            <Text style={styles.name}>{details.name}</Text>
             <Text style={styles.detail}>Location - Distance Away</Text>
-            <Text style={styles.detail}>Available Stock</Text>
+            <Text style={styles.detail}>Available Stock: {stock}</Text>
             <Ionicons
                 style={styles.icon}
                 name="call"
--- client/screens/Patient/MedicineScreen.js
+++ client/screens/Patient/MedicineScreen.js
@@ -1,21 +1,48 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
 import { MedicineScreenDetails } from "../../components/MedicineScreenDetails";
 import { PharmacyCard } from "../../components/PharmacyCard";
 import globalStyles from "../../styles/GlobalStyles";
+import axiosAPI from "../../apis/axiosAPI";
 
 export const MedicineScreen = ({ route }) => {
     const medicine = route.params;
+
+    const [pharmacies, setPharmacies] = useState([]);
+
+    // get pharmacies having this medicine
+    const fetchPharmacies = async () => {
+        try {
+            const res = await axiosAPI.get(
+                `/pharmacies/available/${medicine._id}`
+            );
+            const pharmacies = res.data.pharmacies;
+            return pharmacies;
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        const getData = async () => {
+            const pharmaciesFromServer = await fetchPharmacies();
+            setPharmacies(pharmaciesFromServer);
+        };
+        getData();
+    }, []);
+
     return (
         <View style={globalStyles.pageContainer}>
             <MedicineScreenDetails details={medicine} />
             <Text style={styles.text}>Available at:</Text>
-            <ScrollView style={globalStyles.itemList}>
-                <PharmacyCard />
-                <PharmacyCard />
-                <PharmacyCard />
-                <PharmacyCard />
-            </ScrollView>
+            <FlatList
+                style={globalStyles.itemList}
+                keyExtractor={(item) => item._id}
+                data={pharmacies}
+                renderItem={({ item }) => (
+                    <PharmacyCard details={item} medicine={medicine._id} />
+                )}
+            />
         </View>
     );
 };
