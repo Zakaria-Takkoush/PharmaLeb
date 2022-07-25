8692ff3755137a7082477174de17a82e8999c850
Zakaria <zaktakk96@gmail.com>
2022-07-25 20:36:33 +0300

    Linking: Linked chat screen to firebase chats database using GiftedChat

--- client/screens/Patient/Chat.js
+++ client/screens/Patient/Chat.js
@@ -1,14 +1,70 @@
-import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import { ChatCard } from "../../components/ChatCard";
 import globalStyles from "../../styles/GlobalStyles";
+import {
+    useState,
+    useEffect,
+    useLayoutEffect,
+    useCallback,
+    useContext,
+} from "react";
+import {
+    collection,
+    addDoc,
+    orderBy,
+    query,
+    onSnapshot,
+} from "firebase/firestore";
+import { database } from "../../config/firebase";
+
+import { GiftedChat } from "react-native-gifted-chat";
+
+import { UserContext } from "../../stores/UserContext";
 
 export const Chat = () => {
+    const [messages, setMessages] = useState([]);
+
+    const { userData } = useContext(UserContext);
+
+    useEffect(() => {
+        const collectionRef = collection(database, "chats");
+        const q = query(collectionRef, orderBy("createdAt", "desc"));
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            setMessages(
+                querySnapshot.docs.map((doc) => ({
+                    _id: doc.data()._id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user,
+                }))
+            );
+        });
+        return () => unsubscribe();
+    }, []);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, messages)
+        );
+        const { _id, createdAt, text, user } = messages[0];
+        addDoc(collection(database, "chats"), {
+            _id,
+            createdAt,
+            text,
+            user,
+        });
+    }, []);
+
     return (
-        <SafeAreaView style={globalStyles.pageContainer}>
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-        </SafeAreaView>
+        <GiftedChat
+            messages={messages}
+            showAvatarForEveryMessage={true}
+            onSend={(messages) => onSend(messages)}
+            user={{
+                _id: userData._id,
+                avatar: "https://i.pravatar.cc/300",
+            }}
+        />
     );
 };
