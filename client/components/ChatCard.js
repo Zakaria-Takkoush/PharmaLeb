6a402840e097bb439e25fefe436d49a1af6d67be
Zakaria <zaktakk96@gmail.com>
2022-07-26 14:52:38 +0300

    Frontend: Displayed chat rooms in Chat Tab

--- client/components/ChatCard.js
+++ client/components/ChatCard.js
@@ -1,11 +1,36 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+    collection,
+    getFirestore,
+    onSnapshot,
+    query,
+    orderBy,
+} from "../config/firebase";
+import { Avatar, ListItem } from "@rneui/base";
 
-export const ChatCard = () => {
+export const ChatCard = ({ id, chatName, enterChat }) => {
+    const [chatMessages, setChatMessages] = useState([]);
+    const db = getFirestore();
+
+    useEffect(() =>
+        onSnapshot(
+            query(
+                collection(db, `chats/${id}`, "messages"),
+                orderBy("timestamp", "desc")
+            ),
+            (snapshot) => {
+                setChatMessages(
+                    snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+                );
+            }
+        )
+    );
+
     return (
-        <View style={styles.card}>
-            <Text style={styles.chatName}>ChatCard</Text>
-        </View>
+        <TouchableOpacity style={styles.card}>
+            <Text style={styles.chatName}>{chatName}</Text>
+        </TouchableOpacity>
     );
 };
 
