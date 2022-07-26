3a225023500cd0e3484566efafcff544fd90f5cf
Zakaria <zaktakk96@gmail.com>
2022-07-27 00:20:51 +0300

    Frontend: Installed axios to admin panel and fetched medicines from server

---
+++ admin/src/api/axiosAPI.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export default axios.create({ baseURL: "http://127.0.0.1:4001/v1/api" });
--- admin/src/components/MedicineList.js
+++ admin/src/components/MedicineList.js
@@ -1,11 +1,29 @@
 import React from "react";
+import axiosAPI from "../api/axiosAPI";
 import Medicine from "./Medicine";
-
+import { useState, useEffect } from "react";
 const MedicineList = () => {
+    const [medicines, setMedicines] = useState([]);
+
+    const fetchMedicines = async () => {
+        try {
+            const res = await axiosAPI.get("/medicines");
+            const medicines = await res.data;
+            setMedicines(medicines);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        fetchMedicines();
+    }, []);
+
     return (
         <div className="medicine-list">
-            <Medicine />
-            <Medicine />
+            {medicines.map((medicine) => (
+                <Medicine key={medicine._id} medicine={medicine} />
+            ))}
         </div>
     );
 };
