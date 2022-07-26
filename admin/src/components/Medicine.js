f6a7180d114804c8ffeee0b5d9164c19c3f34205
Zakaria <zaktakk96@gmail.com>
2022-07-27 00:32:00 +0300

    Linking: Linked medicine list and cards in admin panel with backend

--- admin/src/components/Medicine.js
+++ admin/src/components/Medicine.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
-const Medicine = () => {
+const Medicine = ({ medicine }) => {
     return (
         <div className="medicine-card">
-            <div className="image">Image here</div>
-            <div className="name">Medicine Name</div>
-            <div className="code">Medicine Code</div>
-            <div className="dosage">Medicine Dosage</div>
-            <div className="price">Medicine Price</div>
+            <div className="image">
+                <img className="medicine-img" src={medicine.image} />
+            </div>
+            <div className="name">{medicine.name}</div>
+            <div className="code">{medicine.code}</div>
+            <div className="dosage">{medicine.dosage}</div>
+            <div className="price">{medicine.price}</div>
             <div className="edit">
                 <button>
                     Edit <AiFillEdit color="#009FFF" size={18} />
