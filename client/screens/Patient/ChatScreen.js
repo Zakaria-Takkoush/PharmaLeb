f3fd70b69e76b52bbbfbadf72c64e20de1e42da7
Zakaria <zaktakk96@gmail.com>
2022-07-26 19:07:02 +0300

    Frontend: Added loading renderer for chat screen

--- client/screens/Patient/ChatScreen.js
+++ client/screens/Patient/ChatScreen.js
@@ -5,6 +5,7 @@ import {
     View,
     Keyboard,
     TouchableWithoutFeedback,
+    ActivityIndicator,
 } from "react-native";
 import {
     collection,
@@ -16,52 +17,95 @@ import {
     orderBy,
 } from "../../config/firebase";
 import { UserContext } from "../../stores/UserContext";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export const ChatScreen = ({ navigation, route }) => {
-    const [msgInput, setMsgInput] = useState("");
-    const [messages, setMessages] = useState([]);
+    const { userData } = useContext(UserContext);
+
+    const chatID = route.params.id;
+
     const db = getFirestore();
 
-    const { user } = useContext(UserContext);
+    const [msgInput, setMsgInput] = useState("");
+    const [messages, setMessages] = useState([
+        {
+            _id: 0,
+            text: "New chat created.",
+            createdAt: new Date().getTime(),
+            system: true,
+        },
+        {
+            _id: 1,
+            text: `Hello ${userData.first_name}`,
+            createdAt: new Date().getTime(),
+            user: {
+                _id: userData._id,
+                name: userData.first_name,
+            },
+        },
+    ]);
 
     // Send message function
-    const sendMsg = async () => {
-        Keyboard.dismiss();
+    const handleSend = async (newMessage = []) => {
+        const text = messages[0].text;
+        // await addDoc(collection(db, `chats/${route.params.id}`, "messages"), {
+        //     text,
+        //     createdAt: new Date().getTime(),
+        //     user: {
+        //         _id: userData._id,
+        //         email: userData.email,
+        //     },
+        // })
+        //     // .then(() => setMsgInput(""))
+        //     .catch((error) => alert(error.message));
 
-        await addDoc(collection(db, `chats/${route.params.id}`, "messages"), {
-            timestamp: serverTimestamp(),
-            message: msgInput,
-            displayName: user.first_name,
-            email: user.email,
-        })
-            .then(() => setMsgInput(""))
-            .catch((error) => alert(error.message));
+        setMessages(GiftedChat.append(messages, newMessage));
     };
 
-    useEffect(
-        () =>
-            onSnapshot(
-                query(
-                    collection(db, `chats/${route.params.id}`, "messages"),
-                    orderBy("timestamp", "desc")
-                ),
-                (snapshot) => {
-                    setMessages(
-                        snapshot.docs.map((doc) => ({
-                            id: doc.id,
-                            ...doc.data(),
-                        }))
-                    );
-                }
-            ),
-        [route]
-    );
+    // useEffect(
+    //     () =>
+    //         onSnapshot(
+    //             query(
+    //                 collection(db, `chats/${route.params.id}`, "messages"),
+    //                 orderBy("timestamp", "desc")
+    //             ),
+    //             (snapshot) => {
+    //                 setMessages(
+    //                     snapshot.docs.map((doc) => ({
+    //                         id: doc.id,
+    //                         ...doc.data(),
+    //                     }))
+    //                 );
+    //             }
+    //         ),
+    //     [route]
+    // );
+
+    const renderLoading = () => {
+        <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#009FFF" />
+        </View>;
+    };
 
     return (
-        <View>
-            <Text>This is the Chat screen</Text>
-        </View>
+        <GiftedChat
+            messages={messages}
+            onSend={(newMessage) => handleSend(newMessage)}
+            user={{
+                _id: userData._id,
+                name: userData.first_name,
+            }}
+            alwaysShowSend
+            scrollToBottom
+            renderLoading={renderLoading}
+        />
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    loadingContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
