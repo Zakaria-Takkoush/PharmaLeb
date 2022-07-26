2231d7b765e43ad4e388cba6fde6a31cdc913d5d
Zakaria <zaktakk96@gmail.com>
2022-07-26 19:55:49 +0300

    Frontend: Styled admin panel header

--- admin/src/components/Header.js
+++ admin/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import logo from "../assets/logo/logo.png";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className="header">
+            <img className="logo" src={logo} />
+            <h1 className="header-title">Welcome to PharmaLeb Admin Panel</h1>
+        </div>
+    );
 };
 
 export default Header;
