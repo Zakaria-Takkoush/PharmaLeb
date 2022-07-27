a291f9047571af98908d9661b6b60dc32174fc18
Zakaria <zaktakk96@gmail.com>
2022-07-27 17:37:03 +0300

    Frontend: Installed expo notifications and set up notification handler

--- client/App.js
+++ client/App.js
@@ -1,6 +1,16 @@
 import { LoginStack } from "./navigation/LoginStack";
 import UserProvider from "./stores/UserContext";
 
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
+
 export default function App() {
     return (
         <UserProvider>
