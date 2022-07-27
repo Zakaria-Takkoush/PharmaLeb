084181d6513bea0b545d36c7a13bcd0b565c176c
Zakaria <zaktakk96@gmail.com>
2022-07-27 10:19:53 +0300

    Frontend: Moved fetch medicines function to the main App file

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,14 +2,32 @@ import "./App.css";
 import AddMedicine from "./components/AddMedicine";
 import Header from "./components/Header";
 import MedicineList from "./components/MedicineList";
+import axiosAPI from "./api/axiosAPI";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [medicines, setMedicines] = useState([]);
+
+    const fetchMedicines = async () => {
+        try {
+            const res = await axiosAPI.get("/medicines");
+            const medicines = await res.data;
+            setMedicines(medicines);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        fetchMedicines();
+    }, []);
+
     return (
-        <div className="App">
+        <>
             <Header />
-            <AddMedicine />
-            <MedicineList />
-        </div>
+            <AddMedicine medicines={medicines} setMedicines={setMedicines} />
+            <MedicineList medicines={medicines} setMedicines={setMedicines} />
+        </>
     );
 }
 
--- admin/src/components/MedicineList.js
+++ admin/src/components/MedicineList.js
@@ -1,24 +1,7 @@
-import React from "react";
-import axiosAPI from "../api/axiosAPI";
+import React, { useContext } from "react";
 import Medicine from "./Medicine";
-import { useState, useEffect } from "react";
-const MedicineList = () => {
-    const [medicines, setMedicines] = useState([]);
 
-    const fetchMedicines = async () => {
-        try {
-            const res = await axiosAPI.get("/medicines");
-            const medicines = await res.data;
-            setMedicines(medicines);
-        } catch (error) {
-            console.log(error.response.data);
-        }
-    };
-
-    useEffect(() => {
-        fetchMedicines();
-    }, []);
-
+const MedicineList = ({ medicines }) => {
     return (
         <div className="medicine-list">
             {medicines.map((medicine) => (
