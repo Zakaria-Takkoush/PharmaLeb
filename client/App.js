0bb3f11f3ca22ffb0aefb11779604dff43ca6a85
Zakaria <zaktakk96@gmail.com>
2022-08-04 16:53:21 +0300

    Frontend: Wrapped app within pharmacy provider

--- client/App.js
+++ client/App.js
@@ -1,10 +1,13 @@
 import { LoginStack } from "./navigation/LoginStack";
+import PharmacyProvider from "./stores/PharmacyContext";
 import UserProvider from "./stores/UserContext";
 
 export default function App() {
     return (
         <UserProvider>
-            <LoginStack />
+            <PharmacyProvider>
+                <LoginStack />
+            </PharmacyProvider>
         </UserProvider>
     );
 }
