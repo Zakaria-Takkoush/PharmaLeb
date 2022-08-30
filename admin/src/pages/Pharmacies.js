5f7a5bfc2bc6454acfcc1a0370408861a0c5626f
Zakaria <zaktakk96@gmail.com>
2022-08-30 22:32:06 +0300

    Frontend: Fetched pharmacies into Pharmacies page

--- admin/src/pages/Pharmacies.js
+++ admin/src/pages/Pharmacies.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosAPI from "../api/axiosAPI";
 
 const Pharmacies = () => {
-    return <div>Pharmacies</div>;
+    const [pharmacies, setPharmacies] = useState([]);
+
+    const getPharmacies = async () => {
+        try {
+            const res = await axiosAPI.get("/pharmacies");
+            const data = await res.data;
+            setPharmacies(data.pharmacies);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getPharmacies();
+    }, []);
+
+    return (
+        <>
+            {pharmacies.map((pharmacy) => (
+                <p>{pharmacy.name}</p>
+            ))}
+        </>
+    );
 };
 
 export default Pharmacies;
