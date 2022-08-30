63e150900c6cf9d6693ce248d9d366b6df97a396
Zakaria <zaktakk96@gmail.com>
2022-08-30 18:25:14 +0300

    Frontend: Added navigation to admin panel navbar

--- admin/src/components/Navbar.js
+++ admin/src/components/Navbar.js
@@ -2,12 +2,37 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate();
     return (
         <nav className="nav-bar">
-            <button>Home</button>
-            <button>Users</button>
-            <button>Pharmacies</button>
-            <button>Medicines</button>
+            <button
+                onClick={() => {
+                    navigate("/");
+                }}
+            >
+                Home
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/users");
+                }}
+            >
+                Users
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/pharmacies");
+                }}
+            >
+                Pharmacies
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/medicines");
+                }}
+            >
+                Medicines
+            </button>
         </nav>
     );
 };
