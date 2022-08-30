d92cd40a3743df25cd2ca3fdc2b2bbb6ffcb9ad1
Zakaria <zaktakk96@gmail.com>
2022-08-30 17:13:27 +0300

    Frontend: Moved medicine editing panel to Medicines page component

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,44 +1,16 @@
 import "./App.css";
-import AddMedicine from "./components/AddMedicine";
 import Header from "./components/Header";
-import MedicineList from "./components/MedicineList";
-import axiosAPI from "./api/axiosAPI";
-import { useEffect, useState } from "react";
 
 import Home from "./pages/Home";
+import Medicines from "./pages/Medicines";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    const [medicines, setMedicines] = useState([]);
-
-    const fetchMedicines = async () => {
-        try {
-            const res = await axiosAPI.get("/medicines");
-            const medicines = await res.data;
-            setMedicines(medicines);
-        } catch (error) {
-            console.log(error.response.data);
-        }
-    };
-
-    useEffect(() => {
-        fetchMedicines();
-    }, []);
-
     return (
         <>
             <Header />
-            <div className="container">
-                <AddMedicine
-                    medicines={medicines}
-                    setMedicines={setMedicines}
-                />
-                <MedicineList
-                    medicines={medicines}
-                    setMedicines={setMedicines}
-                />
-            </div>
+            <Medicines />
         </>
     );
 }
--- admin/src/pages/Medicines.js
+++ admin/src/pages/Medicines.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axiosAPI from "../api/axiosAPI";
+import AddMedicine from "../components/AddMedicine";
+import MedicineList from "../components/MedicineList";
 
 const Medicines = () => {
-    return <div>Medicines</div>;
+    const [medicines, setMedicines] = useState([]);
+
+    const fetchMedicines = async () => {
+        try {
+            const res = await axiosAPI.get("/medicines");
+            const medicines = await res.data;
+            setMedicines(medicines);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        fetchMedicines();
+    }, []);
+
+    return (
+        <div className="container">
+            <AddMedicine medicines={medicines} setMedicines={setMedicines} />
+            <MedicineList medicines={medicines} setMedicines={setMedicines} />
+        </div>
+    );
 };
 
 export default Medicines;
