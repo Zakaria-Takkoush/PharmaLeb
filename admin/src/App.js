9bbf2893e3f936491df0e54b394c3f9be8a32796
Zakaria <zaktakk96@gmail.com>
2022-08-30 17:27:50 +0300

    Frontend: Created admin panel page routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,14 +3,23 @@ import Header from "./components/Header";
 
 import Home from "./pages/Home";
 import Medicines from "./pages/Medicines";
+import Users from "./pages/Users";
+import Pharmacies from "./pages/Pharmacies";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <>
-            <Header />
-            <Medicines />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/medicines" element={<Medicines />} />
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/pharmacies" element={<Pharmacies />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
