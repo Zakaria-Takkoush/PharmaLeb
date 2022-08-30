67ec13bfc7d23c450fa038a12eb3bcc804544ce1
Zakaria <zaktakk96@gmail.com>
2022-08-30 17:34:57 +0300

    Frontend: Created invalid page component for admin panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,26 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
 
+// pages
 import Home from "./pages/Home";
 import Medicines from "./pages/Medicines";
 import Users from "./pages/Users";
 import Pharmacies from "./pages/Pharmacies";
+import InvalidPage from "./pages/InvalidPage";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/medicines" element={<Medicines />} />
-                    <Route path="/users" element={<Users />} />
-                    <Route path="/pharmacies" element={<Pharmacies />} />
-                </Routes>
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/medicines" element={<Medicines />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/pharmacies" element={<Pharmacies />} />
+                <Route path="*" element={<InvalidPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ admin/src/pages/InvalidPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InvalidPage = () => {
+    return (
+        <div>
+            <h2>Page not Found!</h2>
+        </div>
+    );
+};
+
+export default InvalidPage;
