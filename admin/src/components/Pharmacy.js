3b7d59e156da015159fb416ff01aa5ed93daa232
Zakaria <zaktakk96@gmail.com>
2022-09-02 17:43:36 +0300

    Frontend: Displayed pharmacy data in admin Pharmacy component

--- admin/src/components/Pharmacy.js
+++ admin/src/components/Pharmacy.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Pharmacy = () => {
-    return <div>Pharmacy</div>;
+const Pharmacy = ({ pharmacy }) => {
+    return (
+        <div className="pharmacy-card">
+            <h3>{pharmacy.name}</h3>
+            <p>Address: {pharmacy.address}</p>
+            <p>Phone #: {pharmacy.phone_number}</p>
+        </div>
+    );
 };
 
 export default Pharmacy;
