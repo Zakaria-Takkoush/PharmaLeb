45af46a17067c63c31b436834350ab49f43d88ee
Zakaria <zaktakk96@gmail.com>
2022-09-09 21:12:24 +0300

    Frontend: Fetched users in Users admin page

---
+++ admin/src/pages/Users.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import UsersTable from "../components/UsersTable";
+import axiosAPI from "../api/axiosAPI";
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        try {
+            const res = await axiosAPI.get("/users");
+            const users = await res.data;
+            setUsers(users);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <UsersTable users={users} />
+        </div>
+    );
+};
+
+export default Users;
