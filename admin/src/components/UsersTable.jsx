5d77561de14b04ac631b0d607663db220784dad2
Zakaria <zaktakk96@gmail.com>
2022-09-09 20:47:07 +0300

    Frontend: Created Users table component

---
+++ admin/src/components/UsersTable.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const UsersTable = () => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>User ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Date of Birth</th>
+                    <th>User Type</th>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
+
+export default UsersTable;
