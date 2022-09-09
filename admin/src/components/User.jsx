3da8455bf02eff25770c8cdcf5a899cd2e886e02
Zakaria <zaktakk96@gmail.com>
2022-09-09 21:11:22 +0300

    Frontend: Created User table row component

---
+++ admin/src/components/User.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const User = () => {
+    return (
+        <tr>
+            <td>User ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Phone Number</td>
+            <td>Date of Birth</td>
+            <td>User Type</td>
+        </tr>
+    );
+};
+
+export default User;
